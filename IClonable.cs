27ad88d2dbfee8cca538ca0e9e8f301f69185376
Mohamed Moustafa <92525570+Dev-MohamedMoustafa@users.noreply.github.com>
2021-10-27 14:27:57 +0200

    Add files via upload
    initial commit

---
+++ IClonable.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * this code was written by Mohamed Moustafa
+ * As a preview of his knowledge in Design Patterns
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public interface IClonable
+    {
+        IClonable Clone();
+    }
+}
---
+++ Profile.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * this code was written by Mohamed Moustafa
+ * As a preview of his knowledge in Design Patterns
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class Profile : IClonable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Nationality { get; set; }
+        public IClonable Clone()
+        {
+            return new Profile()
+            {
+                Name = this.Name,
+                Age = this.Age,
+                Nationality = this.Nationality
+            };
+        }
+    }
+}
